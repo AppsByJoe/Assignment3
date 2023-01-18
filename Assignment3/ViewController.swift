a35328548e4f18c9ec42164de08c5741ebbece5e
AppsByJoe <jth0124@auburn.edu>
2023-01-18 17:03:21 -0500

    ready to start/stop music

--- Assignment3/ViewController.swift
+++ Assignment3/ViewController.swift
@@ -11,8 +11,7 @@ class ViewController: UIViewController {
     var timer = Timer()
     let dateFormatter = DateFormatter()
     var countdownTimer = Timer()
-    var secondsLeft : Int = 0
-    var selectedTime = Date();
+    var secondsLeft : Int = 60
     
     @IBOutlet weak var liveClockLabel: UILabel!
     @IBOutlet weak var backgroundImage: UIImageView!
@@ -22,26 +21,21 @@ class ViewController: UIViewController {
     
     @IBAction func userTimePickerChanged(_ sender: UIDatePicker) {
         if (remainingTimeLabel.text == "Remaining Time") {
-            //TODO
-            selectedTime = userTimePicker.date
+            secondsLeft = Int(userTimePicker.countDownDuration)
         }
     }
     
     @IBAction func startTimer(_ sender: UIButton) {
         if (timerButton.titleLabel!.text == "Start Timer" && remainingTimeLabel.text == "Remaining Time") {
-            //TODO: bring hours and minutes out???
-            var hours = Calendar.current.dateComponents([.hour], from: selectedTime).hour!
-            var minutes = Calendar.current.dateComponents([.minute], from: selectedTime).minute!
-            secondsLeft = hours * 3600 + minutes * 60
             
-            //TODO: format date output????
-            remainingTimeLabel.text = "Time Remaining: \(selectedTime)"
+            secondsLeft -= 1
+            formatRemainingTimeLabel()
             
             countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateRemainingTimeLabel) , userInfo: nil, repeats: true)
         } else if (timerButton.titleLabel!.text == "Stop Music") {
             timerButton.titleLabel!.text = "Start Timer"
             remainingTimeLabel.text = "Remaining Time"
-            secondsLeft = 0
+            secondsLeft = 60
             
             //TODO: stop music
         }
@@ -49,27 +43,32 @@ class ViewController: UIViewController {
     
     @objc func updateRemainingTimeLabel() {
         if (secondsLeft > 0) {
-            var hoursRemaining = secondsLeft/3600 > 0 ? "\(secondsLeft/3600)" : "0"
-            var minutesRemaining = (secondsLeft%3600)/60 > 0 ? "\((secondsLeft%3600)/60)" : "0"
-            var secondsRemaining = (secondsLeft%3600)%60 > 0 ? "\((secondsLeft%3600)%60)" : "0"
-            
-            if (Int(hoursRemaining)! < 10) {
-                hoursRemaining = "0" + hoursRemaining
-            }
-            if (Int(minutesRemaining)! < 10) {
-                minutesRemaining = "0" + minutesRemaining
-            }
-            if (Int(secondsRemaining)! < 10) {
-                secondsRemaining = "0" + secondsRemaining
-            }
-            
-            remainingTimeLabel.text = "Time Remaining: \(hoursRemaining):\(minutesRemaining):\(secondsRemaining)"
+            formatRemainingTimeLabel()
+            secondsLeft -= 1
         } else {
             timerButton.titleLabel!.text = "Stop Music"
             //TODO: start music
+            
+            //TODO: Invalidate doesn't allow multiple timers?
             countdownTimer.invalidate()
         }
+    }
+    
+    func formatRemainingTimeLabel() {
+        var hoursRemaining = secondsLeft/3600 > 0 ? "\(secondsLeft/3600)" : "0"
+        var minutesRemaining = (secondsLeft%3600)/60 > 0 ? "\((secondsLeft%3600)/60)" : "0"
+        var secondsRemaining = (secondsLeft%3600)%60 > 0 ? "\((secondsLeft%3600)%60)" : "0"
         
+        if (Int(hoursRemaining)! < 10) {
+            hoursRemaining = "0" + hoursRemaining
+        }
+        if (Int(minutesRemaining)! < 10) {
+            minutesRemaining = "0" + minutesRemaining
+        }
+        if (Int(secondsRemaining)! < 10) {
+            secondsRemaining = "0" + secondsRemaining
+        }
+        remainingTimeLabel.text = "Time Remaining: \(hoursRemaining):\(minutesRemaining):\(secondsRemaining)"
     }
     
     
