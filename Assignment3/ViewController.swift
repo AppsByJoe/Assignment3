8a555346f263b1f20a9abaeb4d8af9c125abae1d
AppsByJoe <jth0124@auburn.edu>
2023-01-18 16:04:04 -0500

    Implemented countdown label functionality, just need to implement music start and stop

--- Assignment3/ViewController.swift
+++ Assignment3/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
     var timer = Timer()
     let dateFormatter = DateFormatter()
     var countdownTimer = Timer()
-    var secondsLeft = 0
+    var secondsLeft : Int = 0
     var selectedTime = Date();
     
     @IBOutlet weak var liveClockLabel: UILabel!
@@ -28,22 +28,45 @@ class ViewController: UIViewController {
     
     @IBAction func startTimer(_ sender: UIButton) {
         if (timerButton.titleLabel!.text == "Start Timer" && remainingTimeLabel.text == "Remaining Time") {
-            remainingTimeLabel.text = "Time Remaining: /(userTimePicker.date)"
-            
             var hours = Calendar.current.dateComponents([.hour], from: selectedTime).hour!
             var minutes = Calendar.current.dateComponents([.minute], from: selectedTime).minute!
             secondsLeft = hours * 3600 + minutes * 60
+            
+            //TODO: format date output????
+            remainingTimeLabel.text = "Time Remaining: /(userTimePicker.date)"
             
             countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateRemainingTimeLabel) , userInfo: nil, repeats: true)
         } else if (timerButton.titleLabel!.text == "Stop Music") {
             timerButton.titleLabel!.text = "Start Timer"
             remainingTimeLabel.text = "Remaining Time"
+            secondsLeft = 0
             
-            //stop music
+            //TODO: stop music
         }
     }
     
     @objc func updateRemainingTimeLabel() {
+        if (secondsLeft > 0) {
+            var hoursRemaining = secondsLeft/3600 > 0 ? "\(secondsLeft/3600)" : "0"
+            var minutesRemaining = (secondsLeft%3600)/60 > 0 ? "\((secondsLeft%3600)/60)" : "0"
+            var secondsRemaining = (secondsLeft%3600)%60 > 0 ? "\((secondsLeft%3600)%60)" : "0"
+            
+            if (Int(hoursRemaining)! < 10) {
+                hoursRemaining = "0" + hoursRemaining
+            }
+            if (Int(minutesRemaining)! < 10) {
+                minutesRemaining = "0" + minutesRemaining
+            }
+            if (Int(secondsRemaining)! < 10) {
+                secondsRemaining = "0" + secondsRemaining
+            }
+            
+            remainingTimeLabel.text = "Time Remaining: \(hoursRemaining):\(minutesRemaining):\(secondsRemaining)"
+        } else {
+            timerButton.titleLabel!.text = "Stop Music"
+            //TODO: start music
+            countdownTimer.invalidate()
+        }
         
     }
     
